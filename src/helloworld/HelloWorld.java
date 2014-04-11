f52799481328f7e0604877082ce8276b0d65f5fc
g12948ss <g12948ss@s30454.tsuda.ac.jp>
2014-04-11 11:14:24 +0900

    Goodbye →　Good Bye　に訂正しました。

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -15,6 +15,6 @@ public class HelloWorld {
      */
     public static void main(String[] args) {
         System.out.println("Hello");
-        System.out.println("Goodbye");
+        System.out.println("Good Bye");
     }
 }
